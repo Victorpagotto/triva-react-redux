56908f5012221093b773e21a081701b6fe3958cb
Victorpagotto <victor.pagotto12@gmail.com>
2022-06-28 16:27:52 -0300

    Adicionado index.

---
+++ src/3-actions/index.js
@@ -0,0 +1,7 @@
+// Aqui é possível reunir todas as actions em um único objeto para facilitar acesso.
+// Importe todas as actions e em seguida as insira no objeto abaixo.
+
+const actions = {
+};
+
+export default actions;
