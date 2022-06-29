d00cf1612427443aea5ed32fed1f9924251c07c0
Matheus leão <kyracraft0@gmail.com>
2022-06-29 16:48:23 -0300

    Componente RankingPlayers

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import RankingPlayers from '../Components/RankingPlayers';
 
 class Ranking extends React.Component {
   render() {
     return (
       <div>
         <h1>Ranking</h1>
+        <RankingPlayers />
 
         <Link to="/Login">
           <button
