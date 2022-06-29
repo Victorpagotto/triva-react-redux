99d7034f8fc910f33aeee4177acf729136f40de7
FaelCaporali <rafaelhon@gmail.com>
2022-06-29 17:18:11 -0300

    ajuste lint

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import GameHeader from '../Components/Header';
+import Header from '../Components/Header';
 
 class Game extends Component {
   render() {
